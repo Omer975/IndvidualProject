0aae4604232c74eccc74eee64bca519113edf5b0
Omer Sozcu <omerfsozcu@gmail.com>
2020-10-12 23:40:51 +0200

    Third version of to do list 1. Menu updated 2. Remove section created 3. How many tasks and how many done created

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -17,15 +17,18 @@ public class UserInterface {
         while(true){
             System.out.println();
             System.out.println("Welcome to ToDoList");
-            /*System.out.println("You have "+ this.todoList.listSize() + " tasks todo and " + this.todoList.listDoneSize()+ " tasks are done!");*/
+            System.out.println("You have "+ this.todoList.taskSize() + " tasks todo and " + this.todoList.taskDoneSize()+ " tasks are done!");
             System.out.println("Pick an Option: ");
+            System.out.println("__________________________________________");
             System.out.println("(1) Show task list (by date or project)");
             System.out.println("(2) Add New Task");
-            System.out.println("(3) Edit Task (Update, Mark as Done, Remove)");
-            System.out.println("(4) Save and Quit");
+            System.out.println("(3) Edit Task (Update, Mark as Done)");
+            System.out.println("(4) Remove Task");
+            System.out.println("(5) Save and Quit");
+            System.out.println("__________________________________________");
             System.out.print("Your Option: ");
             String command = scanner.nextLine();
-            if (command.equals("4")){
+            if (command.equals("5")){
                 System.out.println("Do you want to save and quit?Y/N");
                 String answer = scanner.nextLine();
                 if (answer.equals("Y")) {
@@ -51,22 +54,21 @@ public class UserInterface {
                 String status = scanner.nextLine();
                 this.todoList.addRecord(title, project, d, status);
             }else if (command.equals("1")){
-                /*if (this.todoList.listSize() == 0){
+                if (this.todoList.taskSize() == 0){
                     System.out.println("********************************");
                     System.out.println("You don't have any task for ToDo");
                     System.out.println("********************************");
                     System.out.println();
-                }*/
+                }
                 this.todoList.print();
-            }/*else if (command.equals("3")){
-                System.out.print("Which one is removed? ");
+            }else if (command.equals("4")){
+                System.out.print("Enter Task Index for Remove: ");
                 int toRemove = scanner.nextInt();
                 System.out.println("Your " + toRemove + "- task is removing...");
                 this.todoList.remove(toRemove);
                 System.out.println("Your " + toRemove + "- task is removed...");
                 System.out.println();
-            }*/
-            this.todoList.print();
+            }
         }
     }
 }
